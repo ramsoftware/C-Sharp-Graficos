6f2946864bdf532be2fe1930efb12ab561e36d09
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2022-03-21 17:50:55 -0500

    Add files via upload

---
+++ 11. Videojuegos/03. Evade/Graficos/Graficos/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Graficos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mnuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuInicio = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuReiniciar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerAnimar = new System.Windows.Forms.Timer(this.components);
+            this.mnuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuPrincipal
+            // 
+            this.mnuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuInicio});
+            this.mnuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnuPrincipal.Name = "mnuPrincipal";
+            this.mnuPrincipal.Size = new System.Drawing.Size(587, 24);
+            this.mnuPrincipal.TabIndex = 0;
+            this.mnuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuInicio
+            // 
+            this.mnuInicio.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuReiniciar,
+            this.toolStripMenuItem1,
+            this.mnuSalir});
+            this.mnuInicio.Name = "mnuInicio";
+            this.mnuInicio.Size = new System.Drawing.Size(48, 20);
+            this.mnuInicio.Text = "Inicio";
+            // 
+            // mnuReiniciar
+            // 
+            this.mnuReiniciar.Name = "mnuReiniciar";
+            this.mnuReiniciar.Size = new System.Drawing.Size(119, 22);
+            this.mnuReiniciar.Text = "Reiniciar";
+            this.mnuReiniciar.Click += new System.EventHandler(this.mnuReiniciar_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(116, 6);
+            // 
+            // mnuSalir
+            // 
+            this.mnuSalir.Name = "mnuSalir";
+            this.mnuSalir.Size = new System.Drawing.Size(119, 22);
+            this.mnuSalir.Text = "Salir";
+            // 
+            // timerAnimar
+            // 
+            this.timerAnimar.Enabled = true;
+            this.timerAnimar.Interval = 200;
+            this.timerAnimar.Tick += new System.EventHandler(this.timerAnimar_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 580);
+            this.Controls.Add(this.mnuPrincipal);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mnuPrincipal;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Evadir las rocas que caen";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.mnuPrincipal.ResumeLayout(false);
+            this.mnuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip mnuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuInicio;
+        private System.Windows.Forms.ToolStripMenuItem mnuReiniciar;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSalir;
+        private System.Windows.Forms.Timer timerAnimar;
+    }
+}
+
---
+++ 11. Videojuegos/03. Evade/Graficos/Graficos/Form1.cs
@@ -0,0 +1,107 @@
+﻿//Videojuego: El jugador debe evitar que le caigan las piedras encima
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graficos {
+    public partial class Form1 : Form {
+        //Constantes para calificar cada celda del tablero
+        private const int VACIO = 0;
+        private const int PIEDRA = 1;
+        private const int JUGADOR = 2;
+
+        int[,] Tablero; //Dónde ocurre realmente la acción
+        int PosJugador, MovimientoJugador; //Columna donde está del jugador
+        Random azar; //Único generador de números aleatorios.
+
+        public Form1() {
+            InitializeComponent();
+            azar = new Random();
+            IniciarParametros();
+        }
+
+        private void mnuReiniciar_Click(object sender, EventArgs e) {
+            IniciarParametros();
+            Refresh();
+        }
+
+        public void IniciarParametros() {
+            //Inicializa el tablero Fila,Columna
+            Tablero = new int[50, 30];
+            MovimientoJugador = 0;
+        }
+
+        private void timerAnimar_Tick(object sender, EventArgs e) {
+            MuevePiedras();
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            //Dependiendo de que tecla de flecha presiona el jugador
+            if (e.KeyCode == Keys.Left) MovimientoJugador = -1; //Jugador se mueve a la izquierda
+            if (e.KeyCode == Keys.Right) MovimientoJugador = 1; //Jugador se mueve a la derecha
+            MueveJugador();
+            Refresh();
+        }
+
+        public void MueveJugador() {
+            //Movimiento del jugador
+            if (PosJugador + MovimientoJugador < 0 || PosJugador + MovimientoJugador >= Tablero.GetLength(1)) return;
+
+            Tablero[Tablero.GetLength(0) - 1, PosJugador] = VACIO;
+            PosJugador += MovimientoJugador;
+            Tablero[Tablero.GetLength(0) - 1, PosJugador] = JUGADOR;
+        }
+
+        public void MuevePiedras() {
+
+            //Movimiento de las piedras
+            for (int fila = Tablero.GetLength(0)-1; fila > 0; fila--) {
+                for (int columna = 0; columna < Tablero.GetLength(1); columna++) {
+                    Tablero[fila, columna] = Tablero[fila - 1, columna];
+                }
+            }
+
+            //Verifica si una piedra golpeó al jugador
+            if (Tablero[Tablero.GetLength(0) - 1, PosJugador] == PIEDRA) {
+                timerAnimar.Stop();
+                MessageBox.Show("Ha sido alcanzado por una piedra", "Fin del juego", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                IniciarParametros();
+                timerAnimar.Start();
+                return;
+            }
+            else
+                Tablero[Tablero.GetLength(0) - 1, PosJugador] = JUGADOR;
+
+            //Pone piedras al azar en la primera fila
+            int colPiedra, TotalPiedras = 3;
+            for (colPiedra = 0; colPiedra < Tablero.GetLength(1); colPiedra++)  Tablero[0, colPiedra] = VACIO;
+            for (int piedra = 1; piedra <= TotalPiedras; piedra++) {
+                do {
+                    colPiedra = azar.Next(0, Tablero.GetLength(1));
+                } while (Tablero[0, colPiedra] == PIEDRA);
+                Tablero[0, colPiedra] = PIEDRA;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+            //Tamaño de cada celda
+            int tamFila = 500 / Tablero.GetLength(0);
+            int tamColumna = 500 / Tablero.GetLength(1);
+            int desplaza = 50;
+
+            //Dibuja el arreglo bidimensional
+            for (int Fila = 0; Fila < Tablero.GetLength(0); Fila++) {
+                for (int Columna = 0; Columna < Tablero.GetLength(1); Columna++) {
+                    int UbicaFila = Fila * tamFila + desplaza;
+                    int UbicaColumna = Columna * tamColumna + desplaza;
+                    switch (Tablero[Fila, Columna]) {
+                        case VACIO: e.Graphics.DrawRectangle(Pens.Blue, UbicaColumna, UbicaFila, tamColumna, tamFila); break;
+                        case PIEDRA: e.Graphics.FillRectangle(Brushes.Black, UbicaColumna, UbicaFila, tamColumna, tamFila); break;
+                        case JUGADOR: e.Graphics.FillRectangle(Brushes.Red, UbicaColumna, UbicaFila, tamColumna, tamFila); break;
+                    }
+                }
+            }
+        }
+    }
+}
