7f212eaafc439fd6923fed5916ec1a13f759a1fb
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2022-03-21 16:39:13 -0500

    Gráfico de ecuación polar animada
    La ecuación polar ahora tiene la variable T de tiempo. Lo que la hace animada.

---
+++ 08. PolarAnimado/Graficos/Graficos/Puntos.cs
@@ -0,0 +1,14 @@
+﻿namespace Graficos {
+    internal class Puntos {
+        //Valor X, Y reales de la ecuación
+        public double valorX, valorY;
+
+        //Puntos convertidos a coordenadas enteras de pantalla
+        public int pantallaX, pantallaY;
+
+        public Puntos(double valorX, double valorY) {
+            this.valorX = valorX;
+            this.valorY = valorY;
+        }
+    }
+}
